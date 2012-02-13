0fbff711ccf79192fbde6007d2f6b284bc6620c8
adutze@gmail.com <adutze@gmail.com@43d272a8-912e-83ea-615f-d2626c1f9f5c>
2012-02-13 14:55:56 +0000

    debug

--- trunk/elevator-lab/planner.c
+++ trunk/elevator-lab/planner.c
@@ -43,48 +43,61 @@ static FloorEvent_t getFloorEvent(void);
 static void pushFloorEvent(FloorEvent_t floor);
 //checks if it is possible to stop at floor 2 on the way to floor 1 or 3
 static bool floor2InTheWay(void);
+static FloorEvent_t readFloorEvent(void);
 
 static void plannerTask(void *params) {
 
 	PinEvent ev;
-	FloorEvent_t floor;
+	FloorEvent_t targetfloor, currentfloor;
+  bool floorreached, doors_closed;
 
 	portTickType xLastWakeTime;
 	// Initialise the xLastWakeTime variable with the current time.
 	xLastWakeTime = xTaskGetTickCount();
 
-	for(;;) {
-		while (xQueueReceive(pinEventQueue, &ev, portMAX_DELAY) == pdTRUE) {
+
+	for(;;) {    
+
+		if(xQueueReceive(pinEventQueue, &ev, portMAX_DELAY) == pdTRUE) {
 			switch(ev) {
 
 				case TO_FLOOR_1:
 					pushFloorEvent(FLOOR1);
+          //setCarTargetPosition(TRACKER_FLOOR1_POS);
 					break;	
 
 				case TO_FLOOR_2:
 					pushFloorEvent(FLOOR2);
+          //setCarTargetPosition(TRACKER_FLOOR2_POS);
 					break;
 
 				case TO_FLOOR_3:
 					pushFloorEvent(FLOOR3);
+          //setCarTargetPosition(TRACKER_FLOOR3_POS);
 					break;					 
 				
 				case ARRIVED_AT_FLOOR:
+            floorreached = TRUE;
 					break;
 					
 				case LEFT_FLOOR:
+            floorreached = FALSE;
 					break;
 
 				case DOORS_CLOSED:
+            doors_closed = TRUE;
 					break;
 
 				case DOORS_OPENING:
+            doors_closed = FALSE;
 					break;
 
 				case STOP_PRESSED:
+            setCarMotorStopped(1);
 					break;
 
 				case STOP_RELEASED:
+            setCarMotorStopped(0);
 					break;
 
 				default:
@@ -92,11 +105,38 @@ static void plannerTask(void *params) {
 
 			}
 		}
-	
-		floor = getFloorEvent();
 
-		if (floor != UNKNOWN) {
+    /* lift  */
+    if( doors_closed )
+    {
+      targetfloor = readFloorEvent();
+    
+      switch(targetfloor)
+      {
+        case FLOOR1:
+          setCarTargetPosition(TRACKER_FLOOR1_POS);
+          break;
+        case FLOOR2:
+          setCarTargetPosition(TRACKER_FLOOR2_POS);
+          break;                                   
+        case FLOOR3:
+          setCarTargetPosition(TRACKER_FLOOR3_POS);
+          break;
+  
+        default:
+          break;
+      }
+    }
 
+    //position = getCarPosition();
+    if( floorreached && !doors_closed )
+    {
+       currentfloor = getFloorEvent();
+    }
+    
+
+
+		if (targetfloor != UNKNOWN) {
 		}
 
 		//TODO:
@@ -116,6 +156,19 @@ void setupPlanner(unsigned portBASE_TYPE uxPriority) {
 	floorQueue.lock = xSemaphoreCreateMutex();
 
   xTaskCreate(plannerTask, "planner", 100, NULL, uxPriority, NULL);
+}
+
+FloorEvent_t readFloorEvent(void) {
+	FloorEvent_t floor;
+
+	xSemaphoreTake(floorQueue.lock, portMAX_DELAY);
+
+	//get the first element in the queue
+	floor = floorQueue.floor[0];
+
+	xSemaphoreGive(floorQueue.lock);
+
+	return floor;
 }
 
 //pull a floor request event from the queue
@@ -138,6 +191,7 @@ FloorEvent_t getFloorEvent(void) {
 //insert a floor request event into the queue
 void pushFloorEvent(FloorEvent_t floor) {
 	u8 i;
+  Direction dir = getCarDirection();
 	
 	xSemaphoreTake(floorQueue.lock, portMAX_DELAY);	
 	
@@ -149,8 +203,8 @@ void pushFloorEvent(FloorEvent_t floor) {
 		}
 	}	
 	
-	//if the requested floor is the middle one	
-	if (floor == FLOOR2) {
+	//if the requested floor is the middle one and lift is mooving
+	if ((floor == FLOOR2) && ( dir != Unknown))  {
 		//if it is possible to stop on the way
 		if (floor2InTheWay()) {
 			//stop to the floor - insert floor at the beginning of 	queue
